d8f0b524a43b1f11b0c13845d5efdab625efd0c9
Bobby Clarkson <tivll@live.fr>
2021-01-25 16:08:38 +0100

    Ajoutez des fichiers projet.

---
+++ exercice 21/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entrez le nom d'une ville : ");
+            string ville = (Console.ReadLine());
+            Console.Write("Entrez le nombre d'habitants : ");
+            string habitant = (Console.ReadLine());
+            // affichage du nom de la ville et du nombre d'habitants
+
+            Console.Write(ville + " possède"+habitant+ "habitants.");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ exercice 21/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("exercice 21")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("exercice 21")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("fd28e14c-fcbb-484c-93e4-49e2e452f412")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
